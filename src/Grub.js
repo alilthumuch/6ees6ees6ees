2bae13917c05fd4be15f9ab632f0d5ba00feb263
alilthumuch <alilthumuch@gmail.com>
2018-06-27 08:47:34 -0700

    Bees in ES6

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,14 @@
 class Grub {
+  // TODO..
+  constructor () {
+  		this.age = 0;
+  		this.color = "pink";
+  		this.food = "jelly";
+  };
+
+  eat() {
+  	//do something
+  	return "Mmmmmmmmm jelly";
+  };
+
 };
